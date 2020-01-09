25dc7219760a7522c11c5a44659525316ec4109b
chris <christopher102994@gmail.com>
2020-01-08 19:31:24 -0500

    Update __init__.py
    Remove curl to gist call

--- rootfs/app/ToonamiAftermath/ToonamiAftermath/__init__.py
+++ rootfs/app/ToonamiAftermath/ToonamiAftermath/__init__.py
@@ -22,18 +22,6 @@ def main():
     tools = Tools()
     tools.wrapper(file_name=file_name, channel_list=channel_list, episode_list=episode_list)
 
-    gist_enabled = os.getenv('GIT_ENABLED')
-    if gist_enabled is not None or gist_enabled is 'TRUE':
-        first_run = os.getenv('FIRST_RUN')
-        if first_run is 'TRUE':
-            tools.curl_to_gist('ToonamiAftermath.m3u', '/data/ToonamiAftermath/ToonamiAftermath.m3u')
-            os.environ['FIRST_RUN'] = 'FALSE'
-            
-        tools.curl_to_gist('Toonami_Aftermath_Guide.xml', file_name)
-
-    else:
-        print('Pushing to Gist Is not enabled.')
-
 
 if __name__ == "__main__":
     main()
